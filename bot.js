8f9f3683c07da79dc5ae49a353fb99aca618ec88
Paul <pldobbs@mac.com>
2018-04-12 10:53:06 -0500

    removed auth.json

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 var Discord = require("discord.io");
 var logger = require("winston");
-var auth = require("./auth.json");
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
