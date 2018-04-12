af9f281ccd3af36bbb0b2e66f49915e2bc6f12b4
Paul <pldobbs@mac.com>
2018-04-12 09:38:37 -0500

    Added more ipsum cases

--- bot.js
+++ bot.js
@@ -26,15 +26,69 @@ bot.on("message", function(user, userID, channelID, message, evt) {
 
     args = args.splice(1);
     switch (cmd) {
-      // !ping
-      case "impsum":
+      case "ipsum":
         bot.sendMessage({
           to: channelID,
           message:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscin."
         });
         break;
-      // Just add any case commands if you want to..
+      case "ipsum-p":
+        bot.sendMessage({
+          to: channelID,
+          message:
+            "`<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscin.</p>`"
+        });
+        break;
+      case "ipsum-div":
+        bot.sendMessage({
+          to: channelID,
+          message:
+            "`<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscin.</div>`"
+        });
+        break;
+      case "ipsum-h1":
+        bot.sendMessage({
+          to: channelID,
+          message: "`<h1>Lorem Ipsum</h1>`"
+        });
+        break;
+      case "ipsum-h2":
+        bot.sendMessage({
+          to: channelID,
+          message: "`<h2>Lorem Ipsum</h2>`"
+        });
+        break;
+      case "ipsum-h3":
+        bot.sendMessage({
+          to: channelID,
+          message: "`<h3>Lorem Ipsum</h3>`"
+        });
+        break;
+      case "ipsum-h4":
+        bot.sendMessage({
+          to: channelID,
+          message: "`<h4>Lorem Ipsum</h4>`"
+        });
+        break;
+      case "ipsum-h5":
+        bot.sendMessage({
+          to: channelID,
+          message: "`<h5>Lorem Ipsum</h5>`"
+        });
+        break;
+      case "ipsum-h6":
+        bot.sendMessage({
+          to: channelID,
+          message: "`<h6>Lorem Ipsum</h6>`"
+        });
+        break;
+      case "ipsum-span":
+        bot.sendMessage({
+          to: channelID,
+          message: "`<span>Lorem ipsum dolor sit amet</span>`"
+        });
+        break;
     }
   }
 });
