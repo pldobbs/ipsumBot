345a8d9fdf5070175ec48fa70a65f66742c79549
Paul <pldobbs@mac.com>
2018-04-12 10:40:59 -0500

    added procfile and environment variables for deployment

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ logger.add(logger.transports.Console, {
 logger.level = "debug";
 // Initialize Discord Bot
 var bot = new Discord.Client({
-  token: auth.token,
+  token: process.env.AUTH_TOKEN,
   autorun: true
 });
 bot.on("ready", function(evt) {
