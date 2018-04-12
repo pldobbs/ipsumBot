c604968d10551ec2de6ee2745d35f5e9ca8454e6
pldobbs <pldobbs@mac.com>
2018-04-11 22:16:27 -0500

    first commit

---
+++ bot.js
@@ -0,0 +1,40 @@
+var Discord = require("discord.io");
+var logger = require("winston");
+var auth = require("./auth.json");
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+  colorize: true
+});
+logger.level = "debug";
+// Initialize Discord Bot
+var bot = new Discord.Client({
+  token: auth.token,
+  autorun: true
+});
+bot.on("ready", function(evt) {
+  logger.info("Connected");
+  logger.info("Logged in as: ");
+  logger.info(bot.username + " - (" + bot.id + ")");
+});
+bot.on("message", function(user, userID, channelID, message, evt) {
+  // Our bot needs to know if it will execute a command
+  // It will listen for messages that will start with `!`
+  if (message.substring(0, 1) == "!") {
+    var args = message.substring(1).split(" ");
+    var cmd = args[0];
+
+    args = args.splice(1);
+    switch (cmd) {
+      // !ping
+      case "impsum":
+        bot.sendMessage({
+          to: channelID,
+          message:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscin."
+        });
+        break;
+      // Just add any case commands if you want to..
+    }
+  }
+});
